37608103afb620f1756ba45ea6c446f270d0f56a
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-06-07 23:12:52 +0000

    js html

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 // Created by: Mr. Coxall
 // Created on: June 7 2025
 // This file contains the JS functions for index.html
+
 'use strict'
 
 /**
@@ -11,19 +12,21 @@
  */
 async function getRandomWord() {
   try {
-    // Fetch a random word from the API
-    const resultJSON = await fetch('https://random-word-api.herokuapp.com/word')
+    const resultJSON = await fetch(
+      'https://random-word-api.herokuapp.com/word?'
+    )
     const jsonData = await resultJSON.json()
     console.log(jsonData)
 
-    // The API returns an array, take the first word
-    const randomWord = jsonData[0]
+    // Get a random index based on the length of the array
+    const randomIndex = Math.floor(Math.random() * jsonData.length)
+    const randomWord = jsonData[randomIndex]
 
-    // Output the random word to the page
-    document.getElementById('random-word').innerText = randomWord
+    // Output the random word
+    document.getElementById('random-word').innerHTML =
+      '<b>Random Word:</b> "' + randomWord +'"<br />'
   } catch (error) {
     console.error(error)
-    document.getElementById('random-word').innerText = 'Failed to fetch a word. Please try again.'
   }
 }
 
