e3168e539394a712a79bd5be35fa84467a76a899
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-06-09 23:07:50 +0000

    edited html flowchart story board js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2025 Catherine Vu All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Catherine Vu
 // Created on: June 7 2025
 // This file contains the JS functions for index.html
 
@@ -10,23 +10,32 @@
  * This function gets a random word from the API
  * The 'async' keyword is used because it will take time from the internet to give random word
  */
-async function getRandomWord() {
+// Copyright (c) 2025 Catherine Vu All rights reserved
+//
+// Created by: Catherine Vu
+// Created on: June 7 2025
+// This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function gets a random fun fact from the API
+ * The 'async' keyword is used because it will take time from the internet to get data
+ */
+async function getFunFact() {
   try {
-    const resultJSON = await fetch(
-      'https://random-word-api.herokuapp.com/word?'
+    const response = await fetch(
+      'https://uselessfacts.jsph.pl/api/v2/facts/random?language=en'
     )
-    const jsonData = await resultJSON.json()
-    console.log(jsonData)
+    const data = await response.json()
+    console.log(data)
 
-    // Get a random index based on the length of the array
-    const randomIndex = Math.floor(Math.random() * jsonData.length)
-    const randomWord = jsonData[randomIndex]
-
-    // Output the random word
+    // Output the fun fact to the page
     document.getElementById('random-word').innerHTML =
-      '<b>Random Word:</b> "' + randomWord +'"<br />'
+      '<b>Fun Fact:</b> "' + data.text + '"<br />'
   } catch (error) {
     console.error(error)
+    document.getElementById('random-word').innerHTML =
+      'Sorry! Could not fetch a fun fact at the moment.'
   }
 }
-
